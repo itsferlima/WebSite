b0eb471aeae46e83c745d53cd975053077102fb8
fernanda oliveira <fer.nand.2149@bol.com.br>
2021-07-30 12:26:29 -0300

    Criação de footer

---
+++ footer.php
@@ -0,0 +1,14 @@
+<?php
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
+
+if ( ! function_exists( 'elementor_theme_do_location' ) || ! elementor_theme_do_location( 'footer' ) ) {
+	get_template_part( 'template-parts/footer' );
+}
+?>
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
